a109ffee734a7387e4309cddfe18462fecdd915e
Tomás Ribeiro <77237108+mgnpt@users.noreply.github.com>
2024-02-18 23:29:50 +0000

    Update Morse Code Translator.py

--- Morse Code Translator.py
+++ Morse Code Translator.py
@@ -3,15 +3,20 @@ alf = {
     'G': '--o', 'H': 'oooo', 'I': 'oo', 'J': 'o---', 'K': '-o-', 'L': 'o-oo',
     'M': '--', 'N': '-o', 'O': '---', 'P': 'o--o', 'Q': '--o-', 'R': 'o-o',
     'S': 'ooo', 'T': '-', 'U': 'oo-', 'V': 'ooo-', 'W': 'o--', 'X': '-oo-',
-    'Y': '-o--', 'Z': '--oo', ' ': '/'}
+    'Y': '-o--', 'Z': '--oo', ' ': '/',
+    '1': 'o----', '2': 'oo---', '3': 'ooo--', '4': 'oooo-', '5': 'ooooo',
+    '6': '-oooo', '7': '--ooo', '8': '---oo', '9': '----o', '0': '-----',
+    '.': 'o-oo-oo', ',': '--oo--', '?': 'oo--oo', '!': '-o--o-','(': '-oo-o', ')': '-oo-o-',
+    ':': '---ooo', ';': '-o-oo-', '"': 'o-o-oo'
+        }
 
 q1 = input('Insira o que quer traduzir para código morse: ')
 q1 = q1.upper()
 
-list = []
+mrs_cd = []
 
 for i in q1:
-    list.append(alf[i])
+    mrs_cd.append(alf[i])
 
-for i in list:
+for i in mrs_cd:
     print(i, end=' ')
