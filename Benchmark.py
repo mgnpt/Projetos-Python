9062063e06044799c14aad10ce0ebf70c79ec8b1
Tomás Ribeiro <77237108+mgnpt@users.noreply.github.com>
2024-02-18 17:39:05 +0000

    Update Benchmark.py

--- Benchmark.py
+++ Benchmark.py
@@ -11,23 +11,33 @@ def calc_3():
     for i in range(0, 100000):
         tt = sqrt(i ** 2 // 7 * 35) * 6
 
-q1 = int(input('Qual é o modo de benchmark quer realizar? '))
+while True:
+    q1 = int(input('Qual é o modo de benchmark quer realizar? '))
 
-start = time.time()
+    start = time.time()
 
+    match q1:
+        case 1:
+            calc_1()
+        case 2:
+            calc_2()
+        case 3:
+            calc_3()
+        case _:
+            print('Input inválido')
+            break
 
-match q1:
-    case 1:
-        calc_1()
-    case 2:
-        calc_2()
-    case 3:
-        calc_3()
+    end = time.time()
+    temp = end - start
 
-end = time.time()
-temp = end - start
+    print(f'Para fazer os calculos o computador demorou: {temp} segundos')
 
-print(f'Para fazer os calculos o computador demorou: {temp} segundos')
+    q2 = int(input('Quer continuar?\n1-SIM\n2-NÃO'))
+    if q2 == 2:
+        break
+    elif q2 != 1:
+        print('Input inválido')
+        break
 
 #0.005390644073486328
 #0.012660741806030273
