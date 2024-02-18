ee45a2beddd0df6d03d300721476c6abc02fadbf
Tomás Ribeiro <77237108+mgnpt@users.noreply.github.com>
2024-02-18 17:30:17 +0000

    Add files via upload

---
+++ Benchmark.py
@@ -0,0 +1,31 @@
+import time
+from math import sqrt
+
+def calc_1():
+    for i in range(0, 100000):
+        tt = i ** 2
+def calc_2():
+    for i in range(0, 100000):
+        tt = i ** 2 // 7 * 35
+def calc_3():
+    for i in range(0, 100000):
+        tt = sqrt(i ** 2 // 7 * 35) * 6
+
+q1 = int(input('Qual é o modo de benchmark quer realizar? '))
+
+start = time.time()
+
+
+match q1:
+    case 1:
+        calc_1()
+    case 2:
+        calc_2()
+    case 3:
+        calc_3()
+
+end = time.time()
+temp = end - start
+
+print(f'Para fazer os calculos o computador demorou: {temp} segundos')
+
