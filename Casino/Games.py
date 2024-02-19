faca39b6ca8c40e54b6fb98717feb1f3ce0869bc
Tomás Ribeiro <77237108+mgnpt@users.noreply.github.com>
2024-02-19 18:07:35 +0000

    Update Games.py

--- Casino/Games.py
+++ Casino/Games.py
@@ -9,20 +9,39 @@ def coin_flip():
         resultado = 'coroa'
         print(resultado)
 
+
+def par(a):
+    a % 2 == 0
+def impar(a):
+    a % 2 != 0
+
 def roulette():
     n1 = randint(0, 36)
+    print(n1)
+    cor = ''
+    evorod = ''
     if n1 == 0:
         resultado = '0 - Green'
+        cor = 'Green'
     elif 1 <= n1 <= 10 or 19 <= n1 <= 28:
-        if n1 % 2 == 0:  # even
+        if n1 % 2 == 0:
             resultado = f'{n1} - Preto'
+            cor = 'Preto'
+            evorod = 'Par'
         else:
             resultado = f'{n1} - Vermelho'
-    else:  # 11 to 18 and 29 to 36
-        if n1 % 2 == 0:  # even
+            cor = 'Vermelho'
+            evorod = 'Impar'
+    else:  # 11 até 18 e 29 até 36
+        if n1 % 2 == 0:
             resultado = f'{n1} - Vermelho'
+            cor = 'Vermelho'
+            evorod = 'Par'
         else:
             resultado = f'{n1} - Preto'
+            cor = 'Preto'
+            evorod = 'Impar'
     print(resultado)
-
+    print(evorod)
+#Falta adicionar se é par ou impar, 3 * 2 to 1 e 1 to 18 e 19 to 36
 roulette()
