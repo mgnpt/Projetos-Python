3c410a4e6423a51e7ccb414a711faef29958ca45
Tomás Ribeiro <77237108+mgnpt@users.noreply.github.com>
2024-02-20 17:43:07 +0000

    Update Games.py
    Acabei a roulette, desisti da ideia das funções par/impar e adicionei verificações para ver se o número está: nos primeiros 18 números ou outras , em que grupo de 12 números (3 grupos) e em que coluna está (2to1). Removi ainda uns prints que eram para fins de debugger

--- Casino/Games.py
+++ Casino/Games.py
@@ -9,39 +9,57 @@ def coin_flip():
         resultado = 'coroa'
         print(resultado)
 
-
-def par(a):
-    a % 2 == 0
-def impar(a):
-    a % 2 != 0
-
 def roulette():
     n1 = randint(0, 36)
-    print(n1)
     cor = ''
     evorod = ''
+    One_To_Eighteen = False    #1 até 18
+    Nineteen_To_ThirtySix = False    #19 até 36
+    Fr_Twelve = False    #Primeiros 12 números
+    Sc_Twelve = False    #12 números no meio
+    Th_Twelve = False    #Últimos 12 números
+    Fr_Two_To_One = False    #1º 2to1
+    Sc_Two_To_One = False    #2º 2to1
+    Th_Two_To_One = False    #3º 2to1
     if n1 == 0:
-        resultado = '0 - Green'
+        resultado = '0'
         cor = 'Green'
+        evorod = 'Nenhum'
     elif 1 <= n1 <= 10 or 19 <= n1 <= 28:
         if n1 % 2 == 0:
-            resultado = f'{n1} - Preto'
+            resultado = f'{n1}'
             cor = 'Preto'
             evorod = 'Par'
         else:
-            resultado = f'{n1} - Vermelho'
+            resultado = f'{n1}'
             cor = 'Vermelho'
             evorod = 'Impar'
     else:  # 11 até 18 e 29 até 36
         if n1 % 2 == 0:
-            resultado = f'{n1} - Vermelho'
+            resultado = f'{n1}'
             cor = 'Vermelho'
             evorod = 'Par'
         else:
-            resultado = f'{n1} - Preto'
+            resultado = f'{n1}'
             cor = 'Preto'
             evorod = 'Impar'
-    print(resultado)
-    print(evorod)
-#Falta adicionar se é par ou impar, 3 * 2 to 1 e 1 to 18 e 19 to 36
-roulette()
+    if 1 <= n1 <= 18:     #Verificar se n1 está na 1ª parte ou 2ª
+        One_To_Eighteen = True
+    elif 19 <= n1 <= 36:
+        Nineteen_To_ThirtySix = True
+
+    if 1 <= n1 <= 12:     #Verificar em que grupo de 12 números n1 está
+        Fr_Twelve = True
+    elif 13 <= n1 <= 24:
+        Sc_Twelve = True
+    elif 25 <= n1 <= 36:
+       Th_Twelve = True
+
+    if n1 % 3 == 0:    #Verificar em que 2to1 grupo n1 está
+        Th_Two_To_One = True
+    elif (n1-1) % 3 == 0:
+        Sc_Two_To_One = True
+    else:
+        Fr_Two_To_One = True
+
+    print(f'{resultado} - {cor} - {evorod}')
