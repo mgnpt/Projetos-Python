3937c063d280016fc6f05aaedf442b815aecdd3c
Tomás Ribeiro <77237108+mgnpt@users.noreply.github.com>
2024-12-02 14:20:17 +0000

    Add files via upload

---
+++ cal_SOV.py
@@ -0,0 +1,12 @@
+# Octal to Decimal Conversion
+n = input("Digite um número em octal: ")  
+x = 0
+decimal = 0
+
+for i in n[::-1]:
+    res = int(i) * (16 ** x)  
+    x += 1
+    decimal += res
+    print(f'{i} = {res}')
+    
+print(f'{n} em decimal = {decimal}')
